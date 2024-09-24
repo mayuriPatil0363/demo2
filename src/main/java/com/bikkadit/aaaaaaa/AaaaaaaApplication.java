15757dd3e1759915dbe6f0d32792726237eb94b2
mayuriPatil <mayuripatil0363@gmail.com>
2024-09-24 18:26:38 +0530

    This is Frist commit by Mayuri

---
+++ src/main/java/com/bikkadit/aaaaaaa/AaaaaaaApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadit.aaaaaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaaaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaaaaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikkadit/aaaaaaa/AaaaaaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadit.aaaaaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaaaaaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
